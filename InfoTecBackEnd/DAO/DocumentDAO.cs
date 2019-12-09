a00db6d85f659e62f6c43c0ce9520721f8d98f97
Josue <e-jjmartinezt@neoris.com>
2019-12-09 03:17:41 -0700

    Metodos para actualizar y obtener datos por alumno y asesor

--- InfoTecBackEnd/DAO/DocumentDAO.cs
+++ InfoTecBackEnd/DAO/DocumentDAO.cs
@@ -43,9 +43,29 @@ namespace InfoTecBackEnd.DAO
             conn.Close();
             return "Se creo el Document correctamente";
         }
-
-        public DocumentModel getDocumento(string id)
+        public string UpdateDocument(DocumentModel value)
         {
+            SqlCommand cmd = new SqlCommand("UpdateDocuments", conn);
+            cmd.CommandTimeout = 0;
+            cmd.Parameters.AddWithValue("@AlumnId", value.AlumnId);
+            cmd.Parameters.AddWithValue("@Idtipo", value.Idtipo);
+            cmd.Parameters.AddWithValue("@url", value.url);
+            cmd.Parameters.AddWithValue("@fecha", value.fecha);
+            cmd.Parameters.AddWithValue("@ComentarioAsesor", value.ComentarioAsesor);
+            cmd.Parameters.AddWithValue("@idEstatus", value.idEstatus);
+            cmd.Parameters.AddWithValue("@ComentarioAdmRes", value.ComentarioAdmRes);
+            cmd.Parameters.AddWithValue("@Idasesor", value.Idasesor);
+            cmd.Parameters.AddWithValue("@idadmin", value.idadmin);
+            cmd.CommandType = CommandType.StoredProcedure;
+            conn.Open();
+            SqlDataReader dr = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+            conn.Close();
+            return "Se actualizo el Document correctamente";
+        }
+
+        public List<DocumentModel> getDocumento(string id)
+        {
+            List<DocumentModel> documents = new List<DocumentModel>();
             SqlCommand cmd = new SqlCommand("getDocumento", conn);
             cmd.CommandTimeout = 0;
             cmd.Parameters.AddWithValue("@AlumnoNumControl", id);
@@ -66,11 +86,41 @@ namespace InfoTecBackEnd.DAO
                 documento.ComentarioAdmRes = dr.GetString(dr.GetOrdinal("ComentarioAdmRes"));
                 documento.Idasesor = dr.GetString(dr.GetOrdinal("Idasesor"));
                 documento.idadmin = dr.GetString(dr.GetOrdinal("idadmin"));
+                documents.Add(documento);
 
             }
             conn.Close();
 
-            return documento;
+            return documents;
+        }
+        public List<DocumentModel> getDocumentoAlumnoAsesor(string id)
+        {
+            List<DocumentModel> documents = new List<DocumentModel>();
+            SqlCommand cmd = new SqlCommand("getDocumentoAlumnoAsesor", conn);
+            cmd.CommandTimeout = 0;
+            cmd.Parameters.AddWithValue("@AsesorId", id);
+            cmd.CommandType = CommandType.StoredProcedure;
+            conn.Open();
+            SqlDataReader dr = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+            DocumentModel documento = null;
+
+            while (dr.Read())
+            {
+                documento = new DocumentModel();
+                documento.AlumnId = dr.GetString(dr.GetOrdinal("AlumnoId"));
+                documento.Idtipo = dr.GetInt32(dr.GetOrdinal("Idtipo"));
+                documento.url = dr.GetString(dr.GetOrdinal("url"));
+                documento.fecha = dr.GetDateTime(dr.GetOrdinal("fecha"));
+                documento.ComentarioAsesor = dr.GetString(dr.GetOrdinal("ComentarioAsesor"));
+                documento.idEstatus = dr.GetInt32(dr.GetOrdinal("idEstatus"));
+                documento.ComentarioAdmRes = dr.GetString(dr.GetOrdinal("ComentarioAdmRes"));
+                documento.Idasesor = dr.GetString(dr.GetOrdinal("Idasesor"));
+                documento.idadmin = dr.GetString(dr.GetOrdinal("idadmin"));
+                documents.Add(documento);
+            }
+            conn.Close();
+
+            return documents;
         }
 
     }
